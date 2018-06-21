92b943519c26c99df05a9ccb1b6a0f23e132e465
Achim Köhler <achim.koehler@ifm.com>
2018-06-21 16:07:45 +0200

    updated

--- word/mi_libro/preprocess.py
+++ word/mi_libro/preprocess.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
+import re
+from functools import partial
+import argparse
+
 
 def process_table(linelist):
     itemlist = []
@@ -34,7 +39,7 @@ def process_table(linelist):
     return '\n'.join(itemlist)
 
 
-def prepocess(text):
+def prepocess_table(text):
     OUTTABLE, INTABLE = 0, 1
     mode = OUTTABLE
     textlist = []
@@ -55,11 +60,46 @@ def prepocess(text):
     return '\n'.join(textlist)
 
 
+def prepocess_replacements(text):
+    subdict = OrderedDict([
+            (r"\*\*([^*]+)\*\*", {"r": r"\\textbf{\1}", "flags": 0}),
+            ("__([^_]+)__", {"r": r"\\uline{\1}", "flags": 0}),
+            ("-->", {"r": r"$\\rightarrow$ ", "flags": 0}),
+            ("//(.+?)//", {"r": r"\\textit{\1}", "flags": 0}),
+    ])
+
+    def replfn(r, matchobj):
+        if r.find("footnote") != -1 and matchobj.group(2).count("|") == 2:
+            tr = matchobj.group(2).split("|")
+            try:
+                print("{1}|{0}|{2}".format(tr[0].encode('utf-8'), tr[1].encode('utf-8'), tr[2].encode('utf-8')))
+            except:
+                print(tr)
+                raise
+            return ' ' + matchobj.group(1) + u"\\footnote{{{}}}".format(tr[1])
+        if r.find("section") != -1 and len(matchobj.groups()) > 1 and matchobj.group(2):
+            return matchobj.expand(r) + "\\" + matchobj.group(2)
+        return matchobj.expand(r)
+
+    for key, replacement in subdict.items():
+        rf = partial(replfn, replacement['r'])
+        text = re.sub(key, rf, text, flags=replacement['flags'])
+    return text
+
+
 if __name__ == '__main__':
-    infilename = "t.tex"
-    outfilename = "p_t.tex"
-    with open(infilename, 'r', encoding="utf-8") as fh:
-        intext = fh.read()
-    outtext = prepocess(intext)
-    with open(outfilename, 'w', encoding="utf-8") as fh:
-        fh.write(outtext)
+    parser = argparse.ArgumentParser(description="prepocess LaTeX file")
+    parser.add_argument("infile", help="input file")
+    parser.add_argument("outfile", help="output file")
+    parser.add_argument("-t", action="store_true", help="process tables")
+    parser.add_argument("-r", action="store_true", help="process replacements")
+
+    args = parser.parse_args()
+    with open(args.infile, 'r', encoding="utf-8") as fh:
+        text = fh.read()
+    if args.r:
+        text = prepocess_replacements(text)
+    if args.t:
+        text = prepocess_table(text)
+    with open(args.outfile, 'w', encoding="utf-8") as fh:
+        fh.write(text)
