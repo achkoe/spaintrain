f7953d21477dbe17127e48d7d03691fca1418c23
Achim Köhler <achim.koehler@ifm.com>
2018-07-20 08:02:02 +0200

    updated

--- learn/webtrain/js/main.js
+++ learn/webtrain/js/main.js
@@ -29,7 +29,7 @@ function init() {
         });
         var select = $("<select id='s" + item_id + "'/>")
         select.append( $("<option>" + NEW + "</option>", {value: 0}))
-        for (var i = 0; i < sessionlist.length; i++) {        
+        for (var i = 0; i < sessionlist.length; i++) {
             select.append( $("<option>" + sessionlist[i] + "</option>", {value: i + 1}))
         }
         $( "#toc" ).append( $("<p>").append(link).append(select));
@@ -38,10 +38,10 @@ function init() {
 }
 
 function show_toc() {
-    /*  
+    /*
         Hide current lesson
         Hide control divs
-        Show table of contents 
+        Show table of contents
     */
     $("#d" + g_lesson).hide();
     $("#control_t").hide();
@@ -55,9 +55,10 @@ function show_lesson(lesson) {
         Hide table of contents
         Show current lesson
         Show control divs
-        Reset Show buttons and hide solution 
+        Reset Show buttons and hide solution
     */
     console.log(lesson);
+    var sessionobj
     // get the selected session
     var session = $( "#s" + lesson + " option:selected").text();
     if (session == NEW) {
@@ -65,7 +66,7 @@ function show_lesson(lesson) {
         session = (new Date()).toISOString();
         console.log(session);
         // add session name to localStorage
-        var sessionobj = JSON.parse(localStorage.getItem(lesson))
+        sessionobj = JSON.parse(localStorage.getItem(lesson))
         sessionobj[session] = {}
         localStorage.setItem(lesson, JSON.stringify(sessionobj));
         console.log(localStorage);
@@ -74,6 +75,7 @@ function show_lesson(lesson) {
     }
     // load data from session
 
+    //
     g_lesson = lesson;
     g_session = session;
     // show and hide things
@@ -88,7 +90,29 @@ function show_lesson(lesson) {
 }
 
 function save() {
+    //TODO: set id of note to lesson_counter_x !!!
+    //TODO: set all names of radio buttons belonging to one group to the same
+    //TODO: set labels for all radio buttons, remove value from radio buttons, remove leading numbers from radio buttons where approbiate
     console.log("saving lesson " + g_lesson + " session " + g_session);
+    var lesson = JSON.parse(localStorage.getItem(g_lesson));
+    console.log("lesson=" + lesson +"|")
+    var result = lesson[g_session];
+    console.log("result=" + result +"|")
+    var item, item_id;
+    $("#d" + g_lesson + " input[type=text]").each(function(index, element) {
+        item_id = $(this).attr("id");
+        item = $(this).val();
+        result[item_id] = item;
+        console.log("" + item_id + ": " + item);
+    });
+    $("#d" + g_lesson + " input[type=radio]").each(function(index, element) {
+        item_id = $(this).attr("id");
+        item = $(this).prop("checked")
+        result[item_id] = item;
+        console.log("" + item_id + ": " + item);
+    });
+    lesson[g_session] = result;
+    localStorage.setItem(g_lesson, JSON.stringify(lesson));
 }
 
 function home() {
