0ba2d0c411d89c6769304ad80ac11c075cf1b5cb
achimk <achkoe@online.de>
2018-07-22 19:28:10 +0200

    updated

---
+++ learn/webtrain/process/process.py
@@ -0,0 +1,96 @@
+import os
+import re
+import codecs
+import glob
+
+OUTPUTFOLDER = "html"
+INPUTFOLDER = "rawcontents"
+
+
+def lesson2html(lesson):
+
+    regexp = re.compile(r"\[([^\]]+)\]")
+    with codecs.open(os.path.join(INPUTFOLDER, "lesson{}.txt".format(lesson)), encoding="utf-8") as fh:
+        linelist = fh.read().splitlines()
+    outlist = [u"""<div class="lesson" id="dl{0}"><h1 id="l{0}">{1}</h1>""".format(lesson, linelist[0])]
+    inputlist = []
+    taskindex = -1
+    for line in linelist[1:]:
+        line = line.strip()
+        if line == u"":
+            continue
+        if line[0].isdigit():
+            # headings starts always with number
+            outlist.append(u"""\n<h2>{}</h2>""".format(line))
+        else:
+            # process task
+            # get all occurences of [] in slist
+            slist = regexp.findall(line)
+            if len(slist) == 0:
+                # it is a task without solution
+                outlist.append(u"""<p class="taskns">{}</p>""".format(line))
+            else:
+                taskindex += 1
+                inputlist = []
+                solutionlist = []
+                # replace all [] with get call
+                line = regexp.sub("{}", line)
+                outlist.append(u"""<p class="task">{}</p>""".format(line))
+                for itemindex, item in enumerate(slist):
+                    tid = u"{}_{}_{}".format(lesson, taskindex, itemindex)
+                    # get the index of the form
+                    if item.startswith(":"):
+                        # we need a radio
+                        itemlist = item.split("|")[1:]
+                        # solution are prefixed with a '!'
+                        radiolist = []
+                        for subindex, subitem in enumerate(itemlist):
+                            if subitem.startswith("!"):
+                                # hide solution in output
+                                itemlist[subindex] = subitem[1:]
+                                # append solution in solutionlist
+                                solutionlist.append(itemlist[subindex])
+                            radiolist.append("""<input required="required" type="radio" id="{ttid}" name="{tid}"/>"""
+                                """<label for="{ttid}">{value}</label>""".format(value=itemlist[subindex], ttid="{}_{}".format(tid, subindex), tid=tid))    
+                        inputlist.append("<span class='rbtask'>{}</span>".format("".join(radiolist)))            
+                    else:
+                        # we need a textbox
+                        inputlist.append("""<input required="required" type="text" id="{tid}" name="{tid}" size="{size}"/>""".format(tid=tid, size=len(item)))
+                        solutionlist.append(item)
+                outlist[-1] = outlist[-1].format(*inputlist)
+                outlist.append(u"""<p class="solution"><span class="note">"""
+                    """<input type="radio" id="s{0}_2" name="s{0}"/>"""
+                    """<label for="s{0}_2">&#x2713;</label>""" 
+                    """<input type="radio" id="s{0}_1" name="s{0}"/>"""
+                    """<label for="s{0}_1">&#x274D;</label>""" 
+                    """<input type="radio" id="s{0}_0" name="s{0}"/>"""
+                    """<label for="s{0}_0">&#x2717;</label>""" 
+                    """</span><span class="sc">{1}</span></p>""".format(taskindex, ", ".join(solutionlist)))
+    outlist.append("</div>")
+    return u"\n".join(outlist)
+
+
+def process(lessonlist):
+    for lesson in lessonlist:
+        htmltext = lesson2html(lesson)
+        with codecs.open(os.path.join(OUTPUTFOLDER, "lesson{}.html".format(lesson)), "w", encoding="utf-8") as fh:
+            fh.write(htmltext)
+
+
+def joinall():
+    filelist = sorted(glob.glob(os.path.join(OUTPUTFOLDER, "lesson*.html")))
+    htmllist = []
+    for filename in filelist:
+        with codecs.open(filename, encoding="utf-8") as fh:
+            htmllist.append(fh.read())
+    with codecs.open("template.html", encoding="utf-8") as fh:
+        template = fh.read()
+    with codecs.open(os.path.join("..", "webtrain.html"), "w", encoding="utf-8") as fh:
+        fh.write(template.format(content="\n".join(htmllist)))
+
+
+if __name__ == '__main__':
+    lessonlist = [1, 2, 3, 12, 43]
+    #lessonlist = [12]
+    process(lessonlist)
+    joinall()
