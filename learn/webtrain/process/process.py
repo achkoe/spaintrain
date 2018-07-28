8886bfa6e7866ef4e95ea2f135a486f76eea1817
achimk <achkoe@online.de>
2018-07-28 18:16:51 +0200

    updated

--- learn/webtrain/process/process.py
+++ learn/webtrain/process/process.py
@@ -91,7 +91,7 @@ def joinall(lessonlist):
 
 
 if __name__ == '__main__':
-    lessonlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 43]
+    lessonlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 43]
     #lessonlist = [12]
     process(lessonlist)
     joinall(lessonlist)
