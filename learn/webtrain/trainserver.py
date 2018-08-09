dd364b48fa03e0e2ea39e226532bbee03c561a6b
achimk <achkoe@online.de>
2018-08-09 19:47:18 +0200

    updated

--- learn/webtrain/trainserver.py
+++ learn/webtrain/trainserver.py
@@ -1,28 +1,26 @@
 import web
 import json
-        
+
 urls = (
-    '/(.*)', 'hello'
+    '/(.*)', 'main'
 )
 safename = "webtraindata.json"
 app = web.application(urls, globals())
-with open(safename, "r") as fh:   
+with open(safename, "r") as fh:
     safe = json.load(fh)
 
-class hello:        
+
+class main:
     def POST(self, data):
         data = json.loads(web.data())
-        print "data: ", data
+        # print "data: ", data
         safe.update(data)
-        with open(safename, "w") as fh:   
-            json.dump(safe, fh)             
+        with open(safename, "w") as fh:
+            json.dump(safe, fh)
         web.header('Access-Control-Allow-Origin', '*')
         return json.dumps(safe)
 
 
 if __name__ == "__main__":
     web.internalerror = web.debugerror
-    #print dir(myform()) 
-    #print help(form.AttributeList) 
-    #print form.AttributeList
     app.run()
