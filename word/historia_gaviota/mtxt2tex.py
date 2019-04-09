84a1207186fc8d6f4630e073ccc31e6273b57e35
Achim Köhler <achim.koehler@ifm.com>
2019-04-09 07:21:46 +0200

    updated

--- word/historia_gaviota/mtxt2tex.py
+++ word/historia_gaviota/mtxt2tex.py
@@ -84,6 +84,7 @@ def process(args):
 
 
 def process_replace(text):
+    wordlist = []
     subdict = OrderedDict([
             (ur"“", {"r": ur'"', "flags": 0}),
             (ur"”", {"r": ur'"', "flags": 0}),
@@ -110,7 +111,9 @@ def process_replace(text):
             tr = matchobj.group(2).split("|")
             if matchobj.group(2).count("|") == 2:
                 try:
-                    print("{1}|{0}|{2}".format(tr[0].encode('utf-8'), tr[1].encode('utf-8'), tr[2].encode('utf-8')))
+                    #wordlist.append("{1}|{0}|{2}".format(tr[0].encode('utf-8'), tr[1].encode('utf-8'), tr[2].encode('utf-8')))
+                    wordlist.append((tr[0].encode('utf-8'), tr[1].encode('utf-8'), tr[2].encode('utf-8')))
+                    #print("{1}|{0}|{2}".format(tr[0].encode('utf-8'), tr[1].encode('utf-8'), tr[2].encode('utf-8')))
                 except Exception:
                     print(tr)
                     raise
@@ -122,6 +125,21 @@ def process_replace(text):
     for key, replacement in subdict.iteritems():
         rf = partial(replfn, replacement['r'])
         text = re.sub(key, rf, text, flags=replacement['flags'])
+
+    def cmpfn(a, b):
+        a_ = a[0]
+        b_ = b[0]
+        if a_.startswith("el") or a_.startswith("la"):
+            a_ = a_.split()[-1]
+            print(a_)
+        if b_.startswith("el") or b_.startswith("la"):
+            b_ = b_.split()[-1]
+            print(b_)
+        return cmp(a_.lower(), b_.lower())
+
+    wordlist.sort(cmp=cmpfn)
+    for w in wordlist:
+        print("{0}|{1}|{2}".format(*w))
     return text
 
 
