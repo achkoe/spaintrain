4e76ced8e258f1ed90a8fe69d65b4033c060d86f
achimk <achkoe@online.de>
2019-10-06 07:54:54 +0200

    updated

--- learn/verben2anki.py
+++ learn/verben2anki.py
@@ -167,31 +167,9 @@ def markselected():
         "condicional", "futuro", "imperativoafirmativo", "imperfecto", "indefinido",
         "presente", "subjuntivopresente", "gerundio", "participio"]
     verblist = [
-        u'abrazar', u'abrir', u'acabar', u'acertar', u'acompa\xf1ar', u'acordar',
-        u'acostar', u'actuar', u'adivinar', u'adquirir', u'agradecer', u'ahorrar', u'alcanzar',
-        u'alimentar', u'alojar', u'alquilar', u'amar', u'andar', u'anunciar', u'apagar', u'aparcar',
-        u'aparecer', u'aprender', u'apuntar', u'arreglar', u'averiguar', u'avisar', u'ayudar', u'ba\xf1ar',
-        u'bailar', u'bajar', u'beber', u'besar', u'buscar', u'caber', u'caer', u'cambiar', u'caminar',
-        u'cantar', u'casar', u'cenar', u'cerrar', u'chalar', u'charlar', u'chatear', u'cocinar', u'coger',
-        u'colgar', u'comenzar', u'comer', u'cometer', u'competir', u'comprar', u'comprender', u'comprobar',
-        u'conducir', u'confesar', u'confiar', u'confundir', u'conocer', u'conseguir', u'contar',
-        u'contestar', u'continuar', u'contraer', u'convencer', u'copiar', u'correr', u'cortar', u'creer',
-        u'cuidar', u'da\xf1ar', u'dar', u'deber', u'decidir', u'decir', u'dejar', u'desaparecer',
-        u'desarrollar', u'desayunar', u'descansar', u'describir', u'descubrir', u'desear', u'despedir',
-        u'despertar', u'destruir', u'desviar', u'devolver', u'dibujar', u'dirigir', u'disculpar',
-        u'discutir', u'dise\xf1ar', u'disfrutar', u'divertir', u'dividir', u'doler', u'dormir', u'duchar',
-        u'dudar', u'durar', u'echar', u'empezar', u'encontrar', u'ense\xf1ar', u'entender', u'entrar',
-        u'escribir', u'escuchar', u'esperar', u'estar', u'estudiar', u'evitar', u'extra\xf1ar', u'faltar',
-        u'firmar', u'ganar', u'gastar', u'girar', u'gritar', u'gustar', u'haber', u'hablar', u'hacer',
-        u'imaginar', u'imponer', u'intentar', u'ir', u'jugar', u'lanzar', u'leer', u'levantar', u'llamar',
-        u'llegar', u'llevar', u'medir', u'mentir', u'mirar', u'nacer', u'necesitar', u'o\xedr', u'obedecer',
-        u'obligar', u'ofrecer', u'oler', u'olvidar', u'opinar', u'pagar', u'parar', u'parecer', u'pasar',
-        u'pedir', u'pensar', u'perder', u'poder', u'poner', u'preferir', u'preguntar', u'prohibir',
-        u'quedar', u'querer', u'quitar', u'recibir', u'recoger', u'recomendar', u'reconocer', u'recordar',
-        u'regalar', u'repetir', u'reprochar', u'responder', u'robar', u'romper', u'saber', u'sacar',
-        u'salir', u'seguir', u'sentar', u'sentir', u'ser', u'servir', u'significar', u'so\xf1ar', u'soler',
-        u'sonre\xedr', u'subir', u'suceder', u'tener', u'tomar', u'trabajar', u'traducir', u'unir', u'usar',
-        u'valer', u'vender', u'venir', u'ver', u'viajar', u'visitar', u'vivir', u'volar', u'volver']
+        "explicar", "deletrear", "costar", "vestir", "aconsejar", "cansar", "funcionar",
+        "hartar", "interesar", "limpiar", "mandar", "notar", "ocurrir", "preocupar",
+        "quejar", "reservar", "suponer", "tirar"]
     mark(verblist, tenselist)
 
 
